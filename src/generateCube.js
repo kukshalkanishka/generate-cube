799d9334f8b11b641582fa271d99c5d6dd9cdc39
kanishka kuksal <37463477+kukshalkanishka@users.noreply.github.com>
2018-11-21 07:18:16 +0530

    Introduce generate hollow line function.

--- src/generateCube.js
+++ src/generateCube.js
@@ -2,4 +2,19 @@ const repeat = function(character, times) {
   return new Array(times).fill(character).join("");
 }
 
+const generateHollowLine = function(character, times) {
+  let line = "";
+  let repeatTimes = times % (times -1);
+  repeatTimes = repeatTimes || 1;
+  line = line + (repeat(character, repeatTimes));
+
+  if(times > 1) {
+    line = line + repeat(" ", times -2);
+    line = line + character;
+  }
+
+  return line;
+}
+
 exports.repeat = repeat;
+exports.generateHollowLine = generateHollowLine;
--- test/generateCubeTest.js
+++ test/generateCubeTest.js
@@ -1,10 +1,21 @@
 const assert = require('assert');
 const lib = require('../src/generateCube.js');
-const {repeat} = lib;
+const {repeat,
+  generateHollowLine} = lib;
 
 describe("repeat", function() {
   it("should repeat character given number of times", function() {
     assert.equal(repeat("*", 1),"*");
     assert.equal(repeat("*", 6),"******");
+  });
+  it("should return empty string", function() {
+    assert.equal(repeat("*", 0),"");
+  });
+});
+
+describe("generateHollowLine", function() {
+  it("should return hollow line with provided number of characters", function() {
+    assert.equal(generateHollowLine("*", 1), "*");
+    assert.equal(generateHollowLine("*", 4), "*  *");
   });
 });
